1e329ee87d79de7aa3991d3cc0e9f735607fcce5
pc <adenis.machado@unesp.br>
2024-05-23 22:30:10 -0300

    first commit

---
+++ src/mapInterface/mapInterface.java
@@ -0,0 +1,27 @@
+package mapInterface;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class mapInterface {
+public static void main(String[] args) {
+	 Map<Integer, String> mapaNomes = new HashMap<Integer, String>();
+	    mapaNomes.put(1, "João Delfino");
+	    mapaNomes.put(2, "Maria do Carmo");
+	    mapaNomes.put(4, "Claudinei Silva");
+
+
+	    //resgatando o nome da posição 4
+	    System.out.println("Pela chave"+mapaNomes.get(4));
+	    System.out.println("---------------");
+	    System.out.println(mapaNomes);
+	    System.out.println("---------------");//coluna direita		   
+	    System.out.println("Values/nomes"+mapaNomes.values());
+	    System.out.println("---------------");//coluna esquerda		   
+	    System.out.println("keyset"+mapaNomes.keySet());
+	    System.out.println("---------------");//tudo		   
+	    System.out.println("EntrySet"+mapaNomes.entrySet());
+
+
+}
+}
